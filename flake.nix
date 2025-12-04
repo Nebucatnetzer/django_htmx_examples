0af70179ad45e74c36b84d3ec6220d87382da374
Andreas Zweili <andreas@zweili.ch>
2025-12-04 19:11:35 +0100

    Remove flake-parts

--- flake.nix
+++ flake.nix
@@ -1,58 +1,55 @@
 {
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    nix-libs.url = "git+ssh://git@git.2li.ch/Nebucatnetzer/nix-libs.git";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
-    services-flake.url = "github:juspay/services-flake";
   };
   outputs =
-    { flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.process-compose-flake.flakeModule
-        inputs.nix-libs.lib.pkgs-ouput
-      ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      perSystem =
-        { pkgs, ... }:
-        let
-          myPython = pkgs.python3.override {
-            self = myPython;
-            packageOverrides = pyfinal: _pyprev: {
-              htmx-examples = pyfinal.buildPythonPackage {
-                pname = "htmx-examples";
-                inherit (pyproject.project) version;
-                pyproject = true;
-                src = ./.;
-                propagatedBuildInputs = [ pyfinal.hatchling ];
-              };
-              # An editable package with a script that loads our mutable location
-              htmx-examples-editable = pyfinal.mkPythonEditablePackage {
-                # Inherit project metadata from pyproject.toml
-                pname = pyproject.project.name;
-                inherit (pyproject.project) version;
-                # The editable root passed as a string
-                root = "$DEVENV_ROOT/src"; # Use environment variable expansion at runtime
-              };
-            };
+    { ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+      myPython = pkgs.python3.override {
+        self = myPython;
+        packageOverrides = pyfinal: _pyprev: {
+          htmx-examples = pyfinal.buildPythonPackage {
+            pname = "htmx-examples";
+            inherit (pyproject.project) version;
+            pyproject = true;
+            src = root;
+            propagatedBuildInputs = [ pyfinal.hatchling ];
           };
-          pyproject = pkgs.lib.importTOML ./pyproject.toml;
-          WEBPORT = 8080;
-        in
-        {
-          imports = [
-            (import ./tooling/nix/modules/dev_shell.nix { inherit myPython WEBPORT; })
-            (import ./tooling/nix/modules/packages { inherit myPython pyproject; })
-            (import ./tooling/nix/modules/process-compose {
-              servicesFlake = inputs.services-flake.processComposeModules.default;
-              inherit WEBPORT;
-            })
-          ];
+          # An editable package with a script that loads our mutable location
+          htmx-examples-editable = pyfinal.mkPythonEditablePackage {
+            # Inherit project metadata from pyproject.toml
+            pname = pyproject.project.name;
+            inherit (pyproject.project) version;
+            # The editable root passed as a string
+            root = "$DEVENV_ROOT/src"; # Use environment variable expansion at runtime
+          };
         };
+      };
+      pyproject = pkgs.lib.importTOML ./pyproject.toml;
+      # TODO: filter root so that it only contains the relevant files.
+      root = ./.;
+    in
+    {
+      devShells."${system}".default = import ./tooling/nix/dev_shell { inherit myPython pkgs; };
+      packages."${system}" = {
+        inherit
+          (import ./tooling/nix/packages {
+            inherit
+              myPython
+              pyproject
+              pkgs
+              root
+              ;
+          })
+          app-image
+          ci-tools
+          nginx-image
+          ;
+        process-compose = import ./tooling/nix/dev_shell/process-compose {
+          inherit myPython pkgs;
+        };
+      };
     };
 }
---
+++ tooling/nix/dev_shell/process-compose/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+let
+  start-postgres = import ./postgres.nix { inherit pkgs; };
+in
+pkgs.writeShellApplication {
+  name = "process-compose";
+  runtimeInputs = [
+    pkgs.git
+    pkgs.process-compose
+    start-postgres
+  ];
+  text = ''
+    DEVENV_ROOT=$(git rev-parse --show-toplevel)
+    export DEVENV_ROOT
+    process-compose -U --config ${./process-compose.yml} "$@"
+  '';
+}
